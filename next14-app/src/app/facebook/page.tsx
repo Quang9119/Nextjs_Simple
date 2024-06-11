870bd4dfc7c80ac0aafc0f74687f5110eb9843d7
Quang9119 <nguyenminhquangquangquang@gmail.com>
2024-06-11 19:55:37 +0700

    merge

--- next14-app/src/app/facebook/page.tsx
+++ next14-app/src/app/facebook/page.tsx
@@ -1,9 +1,5 @@
 'use client';
-<<<<<<< HEAD
 import { Button } from 'react-bootstrap';
-=======
-
->>>>>>> 707ab7863bf426f46906266df4d176d73cd48e11
 import { useRouter } from 'next/navigation';
 
 const Facebook = () => {
@@ -15,11 +11,7 @@ const Facebook = () => {
     <div>
       <h1>Facebook Page</h1>
       <div>
-<<<<<<< HEAD
         <Button onClick={handleBtnBack}>Back home</Button>
-=======
-        <button onClick={handleBtnBack}>Back home</button>
->>>>>>> 707ab7863bf426f46906266df4d176d73cd48e11
       </div>
     </div>
   );
